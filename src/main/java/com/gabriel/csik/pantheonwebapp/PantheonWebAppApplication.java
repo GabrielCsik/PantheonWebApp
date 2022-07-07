d99baf0821e799053e6d7a869ca97c69cbaa75d9
Gabriel Csík <gabriel.csik07@gmail.com>
2022-07-07 16:08:00 +0200

    1st commit

---
+++ src/main/java/com/gabriel/csik/pantheonwebapp/PantheonWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.csik.pantheonwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantheonWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PantheonWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabriel/csik/pantheonwebapp/PantheonWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.csik.pantheonwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantheonWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
